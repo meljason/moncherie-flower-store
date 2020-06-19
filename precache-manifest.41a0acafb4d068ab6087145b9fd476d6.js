95da2cf7161638da2edad01ee038aa1748344c1f
Jason Chong <34180725+meljason@users.noreply.github.com>
2020-06-19 19:52:03 -0400

    Updates

---
+++ precache-manifest.41a0acafb4d068ab6087145b9fd476d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2926ea59e88ae19dd34493b4ff5cc9",
+    "url": "/moncherie-flower-store/index.html"
+  },
+  {
+    "revision": "0f2c5f49b8b0bf200c1f",
+    "url": "/moncherie-flower-store/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "c86c388143a3a5a5c3e8",
+    "url": "/moncherie-flower-store/static/css/main.a14d4d25.chunk.css"
+  },
+  {
+    "revision": "0f2c5f49b8b0bf200c1f",
+    "url": "/moncherie-flower-store/static/js/2.3e0f402a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/moncherie-flower-store/static/js/2.3e0f402a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86c388143a3a5a5c3e8",
+    "url": "/moncherie-flower-store/static/js/main.7908cff8.chunk.js"
+  },
+  {
+    "revision": "ce11f8c0f3619604b0bf",
+    "url": "/moncherie-flower-store/static/js/runtime-main.5e97bde2.js"
+  },
+  {
+    "revision": "410797e52ffaa99b8c8c44da9ef300f5",
+    "url": "/moncherie-flower-store/static/media/wecome-flower.410797e5.png"
+  }
+]);
